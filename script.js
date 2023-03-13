32e2328a12383eb872d537c11a8525fd57b9a841
Salah Lazzouni <lazzouni.salah@gmail.com>
2023-03-13 12:47:50 -0700

    Remove code and recreate element mapping.

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const minNumber = document.querySelector('.minNumberInput');
-const maxNumber = document.querySelector('.maxNumberInput');
+const minNumber = document.querySelector('.minNumberInput').value;
+const maxNumber = document.querySelector('.maxNumberInput').value;
+const generateButton = document.querySelector('.generateButton');
 const para = document.querySelector('p');
-const button = document.querySelector('.generateButton');
+
+generateButton.addEventListener('click', () => {
+   let answer = generateRandomNumber(minNumber, maxNumber);
+   para.textContent = answer;
+})
+
+
+
+
+
+
+
+
 
 function generateRandomNumber(min,max){
     return Math.random() * (max - min) + min;
 }
-
-generateRandomNumber(minNumber,maxNumber);
-
-button.addEventListener("click", () => {
-    para.textContent = "Hello";
-})
 
 
 
